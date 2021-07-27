8427151393d3285984d9203225f2ddba5b058d8f
josephmani <josephmani.uae@gmail.com>
2021-07-27 16:33:25 +0530

    Added /updates /delete /hastags endpoints

--- __init__.py
+++ __init__.py
@@ -39,9 +39,9 @@ def create_app():
 	@app.route("/login")
 	def loginpage():
 		cursor = dbconn.cursor()
-		username= 'naveenBIGD'
-		name='NAVEEN'
-		password='1234567'
+		username= 'jes2'
+		name='Jesvin'
+		password='jeee'
 		cursor.execute("SELECT id,pwd from users where uname=%s",(username,))
 		temp= cursor.fetchone()
 		
@@ -59,27 +59,102 @@ def create_app():
 	@app.route("/register")
 	def registration():
 		cursor = dbconn.cursor()
-		username= 'lee45'
-		name='LENOAH CHACKO'
-		password='chackol33'
+		username= 'jes2'
+		name='Jesvin'
+		password='jeee'
 		cursor.execute("INSERT INTO users (name, uname, pwd) VALUES(%s, %s, %s)",(name, username, password))
 		dbconn.commit()
 		return "details entered"
 	
 
 	@app.route("/fillnote")
-	def getnote():
+	def getnotes():
 		cursor = dbconn.cursor()
-		userid= 1
-		ntitle='Gaming at 3'
-		description='Gotta get AIR 1 in temple run'
-		hashtag_ids='mobile, gaming'
+		userid= 3
+		ntitle='gaming'
+		description='cod playtime'
+		hashtag_ids='play'
+		hash_id=hashtag_ids.split(',')
+		
+				
 		cursor.execute("INSERT INTO notes (title,dcp,hids,uid) VALUES(%s, %s, %s, %s)",(ntitle, description, hashtag_ids, userid))
+		dbconn.commit()
+		
+		#to get notesid 
+		cursor = dbconn.cursor()
+		cursor.execute("SELECT MAX(nid) from notes")
+		notesid= cursor.fetchall()[0]
+		dbconn.commit()
+		
+		for hashes in hash_id:	
+				cursor = dbconn.cursor()
+				cursor.execute("INSERT INTO hashtags (label,nid,uid) VALUES (%s, %s, %s)",(hashes, notesid, userid))
+				dbconn.commit()
+		
+		cursor = dbconn.cursor()
 		cursor.execute("SELECT title,dcp,hids from notes where uid= %s",(userid,))
 		values= cursor.fetchall()
-		dbconn.commit()
 		return f"Note Entered:\n{values}"
 		
+	@app.route("/update")
+	def updatenote():
+		cursor = dbconn.cursor()
+		userid=3
+		old_title='gaming'
+		ntitle='Complete Genskill assignment'
+		description='Intro to Javascript'
+		hashtag_ids='study,hustle'
+		hash_id=hashtag_ids.split(',')
+		
+		#get notesid to update
+		cursor.execute("SELECT nid from notes where title=%s and uid=%s",(old_title,userid))
+		notesid= cursor.fetchall()[0]
+		dbconn.commit()
+		
+		cursor = dbconn.cursor()
+		cursor.execute("UPDATE notes SET title=%s, dcp=%s, hids=%s where nid=%s and uid=%s",(ntitle, description, hashtag_ids, notesid, userid))
+		cursor.execute("DELETE FROM hashtags where nid=%s and uid=%s",(notesid, userid))
+		dbconn.commit()
+		
+		
+		for hashes in hash_id:	
+				cursor = dbconn.cursor()
+				cursor.execute("INSERT INTO hashtags (label,nid,uid) VALUES (%s, %s, %s)",(hashes, notesid, userid))
+				dbconn.commit()
+		
+		cursor = dbconn.cursor()
+		cursor.execute("SELECT title,dcp,hids from notes where nid= %s and uid=%s",(notesid,userid))
+		values= cursor.fetchall()
+		dbconn.commit()
+		return f"Note Updated as:\n{values}"	
+		
+	
+	@app.route("/delete")
+	def deletenote():
+		cursor = dbconn.cursor()
+		userid=3
+		old_title='Complete Genskill assignment'
+		
+		#get notesid to delete
+		cursor.execute("SELECT nid from notes where title=%s and uid=%s",(old_title, userid))
+		notesid= cursor.fetchall()[0]
+
+		cursor.execute("DELETE FROM hashtags where nid=%s and uid=%s",(notesid, userid))
+		cursor.execute("DELETE FROM notes where nid=%s and uid=%s",(notesid, userid))
+		
+		dbconn.commit()
+		return "Note Deleted"
+
+
+	
+	@app.route("/hashtags")
+	def gethashtags():
+		cursor = dbconn.cursor()
+		userid= 3
+		cursor.execute("SELECT label from hashtags where uid= %s",(userid,))
+		values= cursor.fetchall()
+		dbconn.commit()
+		return f"Hashtags:\n{values}"					
 	
 	return app	
 
